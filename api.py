2a30515b4ad240b344e0425960a396edb98692ca
Dell <nameless8588@gmail.com>
2026-03-19 07:27:59 +0530

    Fix CORS import

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import APIKeyHeader
 from dotenv import load_dotenv
 from cachetools import TTLCache
